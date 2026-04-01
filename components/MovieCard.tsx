c611720e3dcbb728e0a5e634477be240caab7734
mohamed2678 <mohamedgalal2729@gmail.com>
2026-04-01 09:29:48 +0200

    `Added Image component and configured Next.js to handle remote images from image.tmdb.org`

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 type Movie = {
   id: number;
   title: string;
@@ -10,10 +12,12 @@ type Props = {
 export default function MovieCard({ movie }: Props) {
   return (
     <div className="bg-gray-900 rounded-lg overflow-hidden shadow-lg hover:scale-105 transition">
-      <img
+      <Image
         src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
         alt={movie.title}
-        className="w-full h-[300px] object-cover"
+        className="w-full  object-cover"
+        width={500}
+        height={750}
       />
       <div className="p-2">
         <h2 className="text-white text-sm font-semibold">{movie.title}</h2>
